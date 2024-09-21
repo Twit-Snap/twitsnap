a9cd79e773b6ca38fa576033d1385b38db686145
JM-Pascual <jmpascualosorio@gmail.com>
2024-09-21 18:45:02 -0300

    Feat: Main login screen Now the main login screen shows different auth methods.

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,8 +1,9 @@
 import { router } from 'expo-router';
 import { useAtom } from 'jotai';
-import { View, Image, StyleSheet } from 'react-native';
+import {View, Image, StyleSheet, Text} from 'react-native';
 import { Button } from 'react-native-paper';
 import { authenticatedAtom } from './authAtoms/authAtom';
+import { SocialIcon } from 'react-native-elements'
 
 
 export default function SignIn() {
@@ -10,38 +11,70 @@ export default function SignIn() {
 
     return (
         <View style={styles.container}>
+            <Text style={{ fontSize: 60, fontWeight: "300", textAlign: 'center', marginBottom: 20 }}>
+                Everything you love about Twitter,
+            </Text>
+            <Text style={{ fontSize: 60, fontWeight: 'bold', textAlign: 'center', marginBottom: 20 }}>
+                but better.
+            </Text>
             <View style={styles.logoContainer}>
                 <Image
                     source={require('../assets/images/logo_light.png')}
                     style={styles.logo}
                     resizeMode="contain"
                 />
-                <View style={styles.buttonContainer}>
-                    <Button
-                        icon="account-outline"
-                        mode="contained"
-                        buttonColor={'#000'}
-                        onPress={() => {
-                            // call login API
-                            setIsAuthenticated({ email: 'emailInJWT@gmail.com', username: 'usernameInJWT', name: 'nameInJWT' });
-                            // Navigate after signing in. You may want to tweak this to ensure sign-in is
-                            // successful before navigating.
-                            router.replace('/');
-                        }}>
-                        Sign In
-                    </Button>
-                </View>
             </View>
             <View style={styles.buttonContainer}>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginBottom: 20 }}>Already have an account?</Text>
+                <Button
+                    icon="account-outline"
+                    mode="contained"
+                    buttonColor={'#000'}
+                    style={styles.buttonContent}
+                    onPress={() => {
+                        // call login API
+                        setIsAuthenticated({ email: 'emailInJWT@gmail.com', username: 'usernameInJWT', name: 'nameInJWT' });
+                        // Navigate after signing in. You may want to tweak this to ensure sign-in is
+                        // successful before navigating.
+                        router.replace('/');
+                    }}>
+                    Sign In
+                </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <Text style={{ fontSize: 20, fontWeight: 'bold', textAlign: 'center', marginBottom: 20 }}>New to TwitSnap?</Text>
                 <Button
                     icon="account-plus-outline"
                     mode="contained"
                     buttonColor={'#000'}
+                    style={styles.buttonContent}
                     onPress={() => {
                         router.push('/sign-up');
                     }}>
                     Sign up
                 </Button>
+            </View>
+            <View style={styles.buttonContainer}>
+                <SocialIcon
+                    title="Register with Google account"
+                    button
+                    type="google"
+                    style={styles.buttonContent}
+                    onPress={() => {
+                        router.push('/sign-up');
+                    }}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+            <SocialIcon
+                title="Register with Facebook Account"
+                button
+                type="facebook"
+                style={styles.buttonContent}
+                onPress={() => {
+                    router.push('/sign-up');
+                }}
+            />
             </View>
         </View>
     );
@@ -53,14 +86,23 @@ const styles = StyleSheet.create({
         padding: 10,
     },
     logoContainer: {
-        alignItems: 'center',
-        marginBottom: 20,
+        transform: [{ scale: 8.5 }],
+        position: 'absolute',
+        bottom: 195,
+        left : 195,
+
     },
     logo: {
         width: 150,
         height: 50,
     },
+    buttonContent: {
+        height: 40,
+        width: 350,
+    },
     buttonContainer: {
         alignItems: 'center',
+        marginVertical: 10,
+        transform: [{ translateX: 300 }, {translateY : 50}],
     },
 });
