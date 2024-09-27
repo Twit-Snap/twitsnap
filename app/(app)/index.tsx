18a37b6a74ed60ae18cd66212adecbb181538004
JM-Pascual <jmpascualosorio@gmail.com>
2024-09-27 16:07:29 -0300

    Ref: TwitSnap type Now there's a TwitSnap type and the previous static Twits were deleted.

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -4,35 +4,16 @@ import TweetCard from "../../components/twits/TweetCard";
 import TweetBoxFeed from "@/components/twits/TweetBoxFeed";
 import {useAtom} from "jotai";
 import {authenticatedAtom} from "@/app/authAtoms/authAtom";
+import {TwitSnap} from "@/app/types/TwitSnap";
 
 const feed_images = {
     logo: require('../../assets/images/logo_light.png'),
 }
 
-// Define the Tweet type
-type Tweet = {
-  id: string;
-  author: string;
-  content: string;
-  date: string;
-};
-
-const initialTweets: Tweet[] = [
-  { id: '1', author: 'Sergio Agüero', content: 'Messi is the GOAT! 🐐🇦🇷', date: '2021-08-05' },
-  { id: '2', author: 'Ángel Di María', content: 'Proud to play alongside Leo for Argentina! 🇦🇷⚽', date: '2021-08-04' },
-  { id: '3', author: 'Gerard Piqué', content: 'Missing those Barça days with Messi. What a player! 🔵🔴', date: '2021-08-03' },
-  { id: '4', author: 'Andrés Iniesta', content: 'The magic we created at Camp Nou was unforgettable. #Messi', date: '2021-08-02' },
-  { id: '5', author: 'Salomón Rondón', content: 'Respect to Messi, one of the greatest to ever play the game! 👏', date: '2021-08-01' },
-];
-
-// Define props type for TweetItem
-type TweetItemProps = {
-  author: string;
-  content: string;
-};
+const TwitsInFeed: TwitSnap[] = [];
 
 export default function FeedScreen() {
-  const [tweets, setTweets] = useState(initialTweets);
+  const [tweets, setTweets] = useState(TwitsInFeed);
   const [userData] = useAtom(authenticatedAtom);
 
   return (
@@ -48,19 +29,20 @@ export default function FeedScreen() {
           <TweetBoxFeed onTweetSend={(tweetContent) => {
             const newTweet = {
               id: (tweets.length + 1).toString(),
-              author: userData?.username || "Anonymous",
+              authorUsername: userData?.username || "Anonymous",
+              authorName: userData?.name || "Anonymous",
               content: tweetContent,
               date: new Date().toISOString(),
             };
-            setTweets([...tweets, newTweet].sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()));
+            setTweets([newTweet, ...tweets]);
           }}/>
-          <FlatList<Tweet>
+          <FlatList<TwitSnap>
             data={tweets}
             renderItem={({ item }) => {
                 return (
                     <TweetCard
                         profileImage={''}
-                        username={item.author}
+                        username={item.authorName}
                         content={item.content}
                         date={item.date}
                     />
---
+++ app/types/TwitSnap.ts
@@ -0,0 +1,7 @@
+export type TwitSnap = {
+    id: string;
+    authorName: string;
+    authorUsername: string;
+    content: string;
+    date: string;
+};
