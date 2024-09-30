81d2fbfd46a7a1e7cd1e55a37d160e88b5a012de
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-09-30 20:46:26 -0300

    fix: Missing "name" prop at TweetCard

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -2,44 +2,53 @@ import React from "react";
 import { View, ScrollView, StyleSheet, FlatList } from "react-native";
 import TweetCard from "@/components/twits/TweetCard";
 import ProfileHeader from "@/components/profile/ProfileHeader";
-import { useAtom } from 'jotai';
-import { authenticatedAtom } from '../authAtoms/authAtom';
+import { useAtom } from "jotai";
+import { authenticatedAtom } from "../authAtoms/authAtom";
 
 type Tweet = {
-  id: string;
-  content: string;
-  date: string;
+	id: string;
+	content: string;
+	date: string;
 };
 
-
 export default function ProfileScreen() {
-  const [userData] = useAtom(authenticatedAtom) || { userAuth: null };
+	const [userData] = useAtom(authenticatedAtom) || { userAuth: null };
 
-  return (
-      <View style={styles.container}>
-          <ScrollView>
-            {userData && <ProfileHeader userAuth={userData} bio={"Hi! Welcome to my profile. \nI'm a huge Messi fan!"} profilePhoto={""} bannerPhoto={""} />}
-              <FlatList<Tweet>
-              data={[]}
-              renderItem={({ item }) => {
-                return (
-                    <TweetCard profileImage={""}
-                               username={userData?.username || ""}
-                               content={item.content}
-                               date={item.date}
-                    />
-              );
-              }}
-              scrollEnabled={false}
-              ></FlatList>
-          </ScrollView>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<ScrollView>
+				{userData && (
+					<ProfileHeader
+						userAuth={userData}
+						bio={
+							"Hi! Welcome to my profile. \nI'm a huge Messi fan!"
+						}
+						profilePhoto={""}
+						bannerPhoto={""}
+					/>
+				)}
+				<FlatList<Tweet>
+					data={[]}
+					renderItem={({ item }) => {
+						return (
+							<TweetCard
+								profileImage={""}
+								name={userData?.name || ""}
+								username={userData?.username || ""}
+								content={item.content}
+								date={item.date}
+							/>
+						);
+					}}
+					scrollEnabled={false}></FlatList>
+			</ScrollView>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
+	container: {
+		flex: 1,
+		padding: 20,
+	},
 });
