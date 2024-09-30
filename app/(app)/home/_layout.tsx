897c0fbdb0d9ffe22aa94ebd2715308ad0b6d89d
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-09-30 14:15:29 -0300

    feat: For you / Following buttons

--- app/(app)/home/_layout.tsx
+++ app/(app)/home/_layout.tsx
@@ -1,6 +1,12 @@
 import HomeHeader from "@/components/feed/header";
 import { Slot } from "expo-router";
-import { Dimensions, StyleSheet, View } from "react-native";
+import {
+	Dimensions,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from "react-native";
 
 const window = Dimensions.get("screen");
 
@@ -13,6 +19,23 @@ const styles = StyleSheet.create({
 		height: window.height / 10,
 		width: window.width,
 	},
+	feed_type: {
+		maxHeight: window.height / 20,
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		backgroundColor: "rgb(5 5 5)",
+		paddingBottom: 5,
+		paddingHorizontal: 5,
+	},
+	feed_type_button: {
+		height: window.height / 20,
+		width: window.width / 2 - 10,
+		borderBottomWidth: 2,
+		borderColor: "rgb(3, 165, 252)",
+		color: "rgb(255, 255, 255)",
+		textAlign: "center",
+	},
 });
 
 export default function HomeLayout() {
@@ -22,6 +45,15 @@ export default function HomeLayout() {
 				<View style={styles.header}>
 					<HomeHeader />
 				</View>
+				<View style={styles.feed_type}>
+					<TouchableOpacity onPress={() => {}}>
+						<Text style={styles.feed_type_button}>For you</Text>
+					</TouchableOpacity>
+					<TouchableOpacity onPress={() => {}}>
+						<Text style={styles.feed_type_button}>Following</Text>
+					</TouchableOpacity>
+				</View>
+
 				<Slot />
 			</View>
 		</>
