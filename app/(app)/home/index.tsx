1e9d273c9d407d42394a8518abcb20bdd0e50e25
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-10-01 15:43:57 -0300

    refactor: dont call feedtype handler if state is already true Refactor initial_state to state in FeedTypeProp

--- app/(app)/home/index.tsx
+++ app/(app)/home/index.tsx
@@ -1,197 +1,194 @@
-import React, { useEffect, useState } from "react";
+import { authenticatedAtom } from '@/app/authAtoms/authAtom';
+import { TwitSnap } from '@/app/types/TwitSnap';
+import { showTabsAtom } from '@/atoms/showTabsAtom';
+import FeedType, { IFeedTypeProps } from '@/components/feed/feed_type';
+import TweetBoxFeed from '@/components/twits/TweetBoxFeed';
+import TweetCard from '@/components/twits/TweetCard';
+import { useAtom } from 'jotai';
+import React, { useEffect, useState } from 'react';
 import {
-	FlatList,
-	View,
-	ScrollView,
-	StyleSheet,
-	Dimensions,
-	Animated,
-	Keyboard,
-} from "react-native";
-import TweetCard from "@/components/twits/TweetCard";
-import TweetBoxFeed from "@/components/twits/TweetBoxFeed";
-import { useAtom } from "jotai";
-import { authenticatedAtom } from "@/app/authAtoms/authAtom";
-import { TwitSnap } from "@/app/types/TwitSnap";
-import { IconButton } from "react-native-paper";
-import { showTabsAtom } from "@/atoms/showTabsAtom";
-import FeedType, { IFeedTypeProps } from "@/components/feed/feed_type";
-const axios = require("axios").default;
+  Animated,
+  Dimensions,
+  FlatList,
+  Keyboard,
+  ScrollView,
+  StyleSheet,
+  View
+} from 'react-native';
+import { IconButton } from 'react-native-paper';
+const axios = require('axios').default;
 
 const feed_images = {
-	logo: require("@/assets/images/logo_light.png"),
+  logo: require('@/assets/images/logo_light.png')
 };
 
-const window = Dimensions.get("screen");
+const window = Dimensions.get('screen');
 
 const TwitsInFeed: TwitSnap[] = [];
 
 export default function FeedScreen() {
-	const [userData] = useAtom(authenticatedAtom);
-	const [tweets, setTweets] = useState<TwitSnap[]>(TwitsInFeed);
+  const [userData] = useAtom(authenticatedAtom);
+  const [tweets, setTweets] = useState<TwitSnap[]>(TwitsInFeed);
 
-	const [animatedValue, setAnimatedValue] = useState(
-		new Animated.Value(window.height)
-	);
-	const [isExpanded, setIsExpanded] = useState(false);
+  const [animatedValue, setAnimatedValue] = useState(new Animated.Value(window.height));
+  const [isExpanded, setIsExpanded] = useState(false);
 
-	const [showTabs, setShowTabs] = useAtom(showTabsAtom);
+  const [showTabs, setShowTabs] = useAtom(showTabsAtom);
 
-	const handlePress = () => {
-		setShowTabs(!showTabs);
-		Animated.timing(animatedValue, {
-			toValue: isExpanded ? window.height : 0, // Adjust the height as needed
-			duration: 300, // Animation duration in milliseconds
-			useNativeDriver: true,
-		}).start(({ finished }) => {
-			setIsExpanded(!isExpanded);
-		});
-		Keyboard.dismiss()
-	};
+  const handlePress = () => {
+    setShowTabs(!showTabs);
+    Animated.timing(animatedValue, {
+      toValue: isExpanded ? window.height : 0, // Adjust the height as needed
+      duration: 300, // Animation duration in milliseconds
+      useNativeDriver: true
+    }).start(({ finished }) => {
+      setIsExpanded(!isExpanded);
+    });
+    Keyboard.dismiss();
+  };
 
-	const feed: IFeedTypeProps = {
-		items: [
-			{
-				text: "For you",
-				handler: () => {},
-				initial_state: true,
-			},
-			{
-				text: "Following",
-				handler: () => {},
-				initial_state: false,
-			},
-		],
-	};
+  const feed: IFeedTypeProps = {
+    items: [
+      {
+        text: 'For you',
+        handler: () => {},
+        state: true
+      },
+      {
+        text: 'Following',
+        handler: () => {},
+        state: false
+      }
+    ]
+  };
 
-	//   useEffect(() => {
-	//       const loadTweets = async () => {
-	//           const fetchedTweets = await fetchTweets();
-	//           setTweets(fetchedTweets);
-	//       };
-	//       loadTweets();
-	//   }, []);
+  //   useEffect(() => {
+  //       const loadTweets = async () => {
+  //           const fetchedTweets = await fetchTweets();
+  //           setTweets(fetchedTweets);
+  //       };
+  //       loadTweets();
+  //   }, []);
 
-	var temp: TwitSnap[] = [];
-	for (let index = 0; index < 20; index++) {
-		temp.push({
-			id: `${index}`,
-			user: { userId: index, name: "Sergio Agüero", username: "@kun" },
-			content: "Messi is the GOAT! 🐐🇦🇷",
-			createdAt: "2021-08-05",
-		});
-	}
+  var temp: TwitSnap[] = [];
+  for (let index = 0; index < 20; index++) {
+    temp.push({
+      id: `${index}`,
+      user: { userId: index, name: 'Sergio Agüero', username: '@kun' },
+      content: 'Messi is the GOAT! 🐐🇦🇷',
+      createdAt: '2021-08-05'
+    });
+  }
 
-	useEffect(() => setTweets(temp), []);
+  useEffect(() => setTweets(temp), []);
 
-	const fetchTweets = async (): Promise<TwitSnap[]> => {
-		let tweets: TwitSnap[] = [];
-		try {
-			const response = await axios.get(
-				`${process.env.EXPO_PUBLIC_TWITS_SERVICE_URL}snaps`
-			);
-			tweets = response.data.data;
-			console.log("Tweets fetched: ", tweets);
-		} catch (error: any) {
-			console.error("Error:", error);
-			alert("An error occurred. Please try again later.");
-		}
-		return tweets;
-	};
+  const fetchTweets = async (): Promise<TwitSnap[]> => {
+    let tweets: TwitSnap[] = [];
+    try {
+      const response = await axios.get(`${process.env.EXPO_PUBLIC_TWITS_SERVICE_URL}snaps`);
+      tweets = response.data.data;
+      console.log('Tweets fetched: ', tweets);
+    } catch (error: any) {
+      console.error('Error:', error);
+      alert('An error occurred. Please try again later.');
+    }
+    return tweets;
+  };
 
-	const sendTwit = async (tweetContent: string) => {
-		try {
-			const response = await axios.post(
-				`${process.env.EXPO_PUBLIC_TWITS_SERVICE_URL}snaps`,
-				{
-					authorId: userData?.id,
-					authorName: userData?.name,
-					authorUsername: userData?.username,
-					content: tweetContent,
-				},
-				{
-					headers: { "Content-Type": "application/json" },
-				}
-			);
-			console.log("Twit sent: ", response.data);
-		} catch (error: any) {
-			console.error("Error:", error);
-			alert("An error occurred. Please try again later.");
-		}
-	};
+  const sendTwit = async (tweetContent: string) => {
+    try {
+      const response = await axios.post(
+        `${process.env.EXPO_PUBLIC_TWITS_SERVICE_URL}snaps`,
+        {
+          authorId: userData?.id,
+          authorName: userData?.name,
+          authorUsername: userData?.username,
+          content: tweetContent
+        },
+        {
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
+      console.log('Twit sent: ', response.data);
+    } catch (error: any) {
+      console.error('Error:', error);
+      alert('An error occurred. Please try again later.');
+    }
+  };
 
-	return (
-		<>
-			<FeedType {...feed} />
-			<View style={styles.container}>
-				<ScrollView>
-					<FlatList<TwitSnap>
-						data={tweets}
-						renderItem={({ item }) => {
-							return (
-								<TweetCard
-									profileImage={""}
-									name={item.user.name}
-									username={item.user.username}
-									content={item.content}
-									date={item.createdAt}
-								/>
-							);
-						}}
-						keyExtractor={(item) => item.id}
-						scrollEnabled={false}
-					/>
-				</ScrollView>
-			</View>
-			<IconButton
-				icon="plus"
-				style={{
-					backgroundColor: "rgb(3, 165, 252)",
-					zIndex: 10,
-					alignSelf: "flex-end",
-					width: 55,
-					height: 55,
-					borderRadius: 200,
-					position: "absolute",
-					top: window.height * 0.83,
-					right: 15,
-				}}
-				iconColor="rgb(255 255 255)"
-				onPress={handlePress}
-			/>
-			<Animated.View
-				style={[
-					{
-						backgroundColor: "rgb(5 5 5)",
-						zIndex: 50,
-						position: "absolute",
-						bottom: 0,
-						top: 0,
-						paddingTop: 35,
-						width: window.width,
-					},
-					{
-						transform: [{ translateY: animatedValue }],
-						bottom: 0,
-					},
-				]}>
-				<View style={{ height: window.height }}>
-					<TweetBoxFeed
-						onTweetSend={(tweetContent) => {
-							sendTwit(tweetContent);
-						}}
-						onClose={handlePress}
-					/>
-				</View>
-			</Animated.View>
-		</>
-	);
+  return (
+    <>
+      <FeedType {...feed} />
+      <View style={styles.container}>
+        <ScrollView>
+          <FlatList<TwitSnap>
+            data={tweets}
+            renderItem={({ item }) => {
+              return (
+                <TweetCard
+                  profileImage={''}
+                  name={item.user.name}
+                  username={item.user.username}
+                  content={item.content}
+                  date={item.createdAt}
+                />
+              );
+            }}
+            keyExtractor={(item) => item.id}
+            scrollEnabled={false}
+          />
+        </ScrollView>
+      </View>
+      <IconButton
+        icon="plus"
+        style={{
+          backgroundColor: 'rgb(3, 165, 252)',
+          zIndex: 10,
+          alignSelf: 'flex-end',
+          width: 55,
+          height: 55,
+          borderRadius: 200,
+          position: 'absolute',
+          top: window.height * 0.83,
+          right: 15
+        }}
+        iconColor="rgb(255 255 255)"
+        onPress={handlePress}
+      />
+      <Animated.View
+        style={[
+          {
+            backgroundColor: 'rgb(5 5 5)',
+            zIndex: 50,
+            position: 'absolute',
+            bottom: 0,
+            top: 0,
+            paddingTop: 35,
+            width: window.width
+          },
+          {
+            transform: [{ translateY: animatedValue }],
+            bottom: 0
+          }
+        ]}
+      >
+        <View style={{ height: window.height }}>
+          <TweetBoxFeed
+            onTweetSend={(tweetContent) => {
+              sendTwit(tweetContent);
+            }}
+            onClose={handlePress}
+          />
+        </View>
+      </Animated.View>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		flexDirection: "column",
-		backgroundColor: "rgb(5 5 5)"
-	},
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'rgb(5 5 5)'
+  }
 });
--- components/feed/feed_type.tsx
+++ components/feed/feed_type.tsx
@@ -1,84 +1,82 @@
-import React, { useEffect, useState } from "react";
-import { TouchableOpacity, View } from "react-native";
-import { Text } from "react-native";
-import { Dimensions } from "react-native";
-import { StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-const window = Dimensions.get("window");
+const window = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-	feed_type: {
-		maxHeight: window.height / 20 + 10,
-		flex: 1,
-		flexDirection: "row",
-		justifyContent: "space-between",
-		backgroundColor: "rgb(5 5 5)",
-	},
-	feed_type_container: {
-        flex: 1,
-		maxHeight: window.height / 20,
-		paddingHorizontal: 5,
-	},
-	feed_type_button: {
-		height: window.height / 20,
-		color: "rgb(255, 255, 255)",
-		textAlign: "center",
-	},
-	feed_type_selected: {
-		height: 1,
-		borderColor: "rgb(3, 165, 252)",
-		borderRadius: 10,
-	},
+  feed_type: {
+    maxHeight: window.height / 20 + 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'rgb(5 5 5)'
+  },
+  feed_type_container: {
+    flex: 1,
+    maxHeight: window.height / 20,
+    paddingHorizontal: 5
+  },
+  feed_type_button: {
+    height: window.height / 20,
+    color: 'rgb(255, 255, 255)',
+    textAlign: 'center'
+  },
+  feed_type_selected: {
+    height: 1,
+    borderColor: 'rgb(3, 165, 252)',
+    borderRadius: 10
+  }
 });
 
 export interface FeedTypeProp {
-	text: string;
-	initial_state: boolean;
-	handler: () => undefined;
+  text: string;
+  state: boolean;
+  handler: () => undefined;
 }
 
 export interface IFeedTypeProps {
-	items: FeedTypeProp[];
+  items: FeedTypeProp[];
 }
 
 export default function FeedType(props: IFeedTypeProps) {
-	const [items, setItems] = useState<IFeedTypeProps>(props);
+  const [items, setItems] = useState<IFeedTypeProps>(props);
 
-	const handlePress = (this_item: FeedTypeProp) => {
-		setItems({
-			items: items.items.map((item) => {
-				if (this_item === item) {
-					item.initial_state = true;
-					item.handler();
-					return item;
-				}
-                
-				item.initial_state = false;
-				return item;
-			}),
-		});
-	};
+  const handlePress = (this_item: FeedTypeProp) => {
+    setItems({
+      items: items.items.map((item) => {
+        if (this_item === item) {
+          if (!item.state) {
+            item.state = true;
+            item.handler();
+          }
+          return item;
+        }
 
-	return (
-		<View style={styles.feed_type}>
-			{items.items.map((item) => {
-				return (
-					<TouchableOpacity
-						key={item.text}
-						style={styles.feed_type_container}
-						onPress={() => handlePress(item)}>
-						<Text style={styles.feed_type_button}>{item.text}</Text>
-						<Text
-							style={[
-								styles.feed_type_selected,
-								item.initial_state
-									? { borderWidth: 1.5 }
-									: { borderWidth: 0 },
-							]}
-						/>
-					</TouchableOpacity>
-				);
-			})}
-		</View>
-	);
+        item.state = false;
+        return item;
+      })
+    });
+  };
+
+  return (
+    <View style={styles.feed_type}>
+      {items.items.map((item) => {
+        return (
+          <TouchableOpacity
+            key={item.text}
+            style={styles.feed_type_container}
+            onPress={() => handlePress(item)}
+          >
+            <Text style={styles.feed_type_button}>{item.text}</Text>
+            <Text
+              style={[
+                styles.feed_type_selected,
+                item.state ? { borderWidth: 1.5 } : { borderWidth: 0 }
+              ]}
+            />
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+  );
 }
