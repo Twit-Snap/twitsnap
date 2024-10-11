e70567ee5c5816b35aa84b0a45a45044cc169aed
Tinchocw <mcwikla@fi.uba.ar>
2024-10-11 14:23:35 -0300

    Refactor: View of profile to accept own and public profile

--- app/(app)/profile.tsx
+++ app/(app)/profile.tsx
@@ -1,54 +1,95 @@
-import React from "react";
-import { View, ScrollView, StyleSheet, FlatList } from "react-native";
-import TweetCard from "@/components/twits/TweetCard";
-import ProfileHeader from "@/components/profile/ProfileHeader";
-import { useAtom } from "jotai";
-import { authenticatedAtom } from "../authAtoms/authAtom";
+// ProfileScreen.tsx
+import axios from 'axios';
+import { useLocalSearchParams } from 'expo-router';
+import { useAtom } from 'jotai';
+import React, { useEffect, useState } from 'react';
+import { View, ScrollView, StyleSheet, FlatList, ActivityIndicator, Text } from 'react-native';
 
-type Tweet = {
-	id: string;
-	content: string;
-	date: string;
-};
+import { TwitSnap } from '@/app/types/TwitSnap';
+import { SearchedUser } from '@/app/types/publicUser';
+import ProfileHeader from '@/components/profile/ProfileHeader';
+import TweetCard from '@/components/twits/TweetCard';
+
+import { authenticatedAtom } from '../authAtoms/authAtom';
 
 export default function ProfileScreen() {
-	const [userData] = useAtom(authenticatedAtom) || { userAuth: null };
+  const [userData] = useAtom(authenticatedAtom);
+  const { username } = useLocalSearchParams<{ username: string }>();
 
-	return (
-		<View style={styles.container}>
-			<ScrollView>
-				{userData && (
-					<ProfileHeader
-						userAuth={userData}
-						bio={
-							"Hi! Welcome to my profile. \nI'm a huge Messi fan!"
-						}
-						profilePhoto={""}
-						bannerPhoto={""}
-					/>
-				)}
-				<FlatList<Tweet>
-					data={[]}
-					renderItem={({ item }) => {
-						return (
-							<TweetCard
-								profileImage={""}
-								name={userData?.name || ""}
-								username={userData?.username || ""}
-								content={item.content}
-								date={item.date}
-							/>
-						);
-					}}
-					scrollEnabled={false}></FlatList>
-			</ScrollView>
-		</View>
-	);
+  const [searchUserData, setSearchUserData] = useState<SearchedUser | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (!userData || !userData.token) {
+        console.error('No token found.');
+        return; // You can choose to handle this case appropriately (e.g., redirect to login)
+      }
+
+      if (username) {
+        try {
+          const response = await axios.get(
+            `${process.env.EXPO_PUBLIC_USER_SERVICE_URL}users/${username}`,
+            {
+              headers: {
+                Authorization: `Bearer ${userData.token}`
+              }
+            }
+          );
+          setSearchUserData(response.data.data);
+        } catch (error) {
+          console.error('Error fetching user data:', error);
+        }
+      }
+      setLoading(false);
+    };
+
+    fetchUserData();
+  }, [username]);
+
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        {loading ? (
+          <ActivityIndicator size="large" color="white" />
+        ) : searchUserData ? (
+          <ProfileHeader
+            user={searchUserData}
+            bio={"Hi! Welcome to my profile. \nI'm a huge Messi fan!"}
+            profilePhoto={/*searchUserData.profilePhoto || */ ''}
+            bannerPhoto={/*searchUserData.bannerPhoto ||*/ ''}
+          />
+        ) : (
+          <Text style={styles.errorText}>User not found</Text>
+        )}
+        <FlatList<TwitSnap>
+          data={searchUserData?.twits || []}
+          renderItem={({ item }) => (
+            <TweetCard
+              profileImage={''}
+              name={searchUserData?.name || ''}
+              username={searchUserData?.username || ''}
+              content={item.content}
+              date={item.createdAt}
+            />
+          )}
+          scrollEnabled={false}
+        />
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		padding: 20,
-	},
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: 'rgb(5 5 5)'
+  },
+  errorText: {
+    color: 'white',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 20
+  }
 });
--- components/profile/ProfileHeader.tsx
+++ components/profile/ProfileHeader.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 
 import { UserAuth } from '@/app/types/authTypes';
+import { SearchedUser } from '@/app/types/publicUser';
+import { format } from 'date-fns';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 interface IProfileHeader {
-  userAuth: UserAuth;
+  user: SearchedUser;
   bannerPhoto: string;
   profilePhoto: string;
   bio: string;
@@ -18,7 +20,11 @@ const default_images = {
   bannerPhoto: require('../../assets/images/kanagawa.jpg')
 };
 
-const ProfileHeader: React.FC<IProfileHeader> = ({ userAuth, bannerPhoto, profilePhoto, bio }) => {
+const ProfileHeader: React.FC<IProfileHeader> = ({ user, bannerPhoto, profilePhoto, bio }) => {
+
+  const formattedBirthdate = user.birthdate ? format(new Date(user.birthdate), 'MMMM dd, yyyy') : null;
+  const formattedJoinDate = user.createdAt ? format(new Date(user.createdAt), 'MMMM yyyy') : null;
+
   return (
     <View>
       <Image
@@ -31,8 +37,20 @@ const ProfileHeader: React.FC<IProfileHeader> = ({ userAuth, bannerPhoto, profil
             source={profilePhoto ? { uri: profilePhoto } : default_images.profilePhoto}
             style={styles.profilePhoto}
           ></Image>
-          {userAuth && <Text style={styles.name}>{userAuth.name}</Text>}
-          {userAuth && <Text style={styles.username}>@{userAuth.username}</Text>}
+          {user && <Text style={styles.name}>{user.name}</Text>}
+          {user && <Text style={styles.username}>@{user.username}</Text>}
+
+          {/* Birthday and Join Date */}
+          {formattedBirthdate && (
+            <Text style={styles.birthdate}>
+              🎂 Born {formattedBirthdate}
+            </Text>
+          )}
+          {formattedJoinDate && (
+            <Text style={styles.joinDate}>
+              📅 Joined {formattedJoinDate}
+            </Text>
+          )}
         </View>
       </View>
       <Text style={styles.bio}>{bio}</Text>
@@ -62,14 +80,15 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 18,
+    color: 'white',
     fontWeight: 'bold',
-    marginTop: -75,
-    marginLeft: 25
+    marginTop: -70,
+    marginLeft: 5
   },
   username: {
     fontSize: 16,
-    color: '#666',
-    marginLeft: 25
+    color: 'grey',
+    marginLeft: 5
   },
   bio: {
     fontSize: 16,
@@ -79,6 +98,16 @@ const styles = StyleSheet.create({
   textContainer: {
     flex: 1,
     justifyContent: 'center'
+  },
+  birthdate: {
+    fontSize: 14,
+    color: '#939090',
+    marginTop: 23
+  },
+  joinDate: {
+    fontSize: 14,
+    color: '#939090',
+    marginTop: 10
   }
 });
 
