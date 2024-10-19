262cc2e342777a4a7319650d5cbca1b7acf57ccb
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-10-19 20:04:29 -0300

    feat: add searcheduser attributes

--- app/types/publicUser.ts
+++ app/types/publicUser.ts
@@ -1,10 +1,14 @@
 import { TwitSnap } from '@/app/types/TwitSnap';
 
 export interface SearchedUser {
+  id: number;
   username: string;
   name: string;
+  description: string;
   birthdate: string;
   createdAt: string;
+  profileImage: string;
+  backgroundImage: string;
   twits: TwitSnap[];
   following: boolean;
   followingCount: number;
