e8469496ceeab86f8df96276b1ca2e7a44bdbc9a
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-10-21 00:16:19 -0300

    fix: merge error

--- app/(app)/searchResults.tsx
+++ app/(app)/searchResults.tsx
@@ -136,6 +136,8 @@ export default function SearchResultsScreen() {
               keyExtractor={(item) => item.id.toString()}
             />
           </View>
+        ) : (
+          <></>
         )
       ) : (
         <></>
