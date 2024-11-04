cbfc45deec20919a0b8a95f0aef7c4f9b400b947
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-04 16:40:37 -0300

    feat: post and delete retwit

--- components/twits/Interactions/retwit.tsx
+++ components/twits/Interactions/retwit.tsx
@@ -1,4 +1,6 @@
+import { authenticatedAtom } from '@/app/authAtoms/authAtom';
 import useAxiosInstance from '@/hooks/useAxios';
+import { useAtomValue } from 'jotai';
 import React from 'react';
 import Interaction, { handlerReturn } from '../interaction';
 
@@ -12,6 +14,61 @@ export default function Retwit({
   twitId: string;
 }) {
   const axiosTwits = useAxiosInstance('twits');
+  const userData = useAtomValue(authenticatedAtom);
+
+  const postRetwit = async (state?: boolean, count?: number): Promise<handlerReturn> => {
+    const newState =
+      (await axiosTwits
+        .post(
+          `snaps`,
+          {
+            user: {
+              userId: userData?.id,
+              name: userData?.name,
+              username: userData?.username
+            },
+            content: '',
+            type: 'retwit',
+            parent: twitId
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+        )
+        .then(() => !state)
+        .catch(() => {
+          return state;
+        })) === true;
+
+    return {
+      state: newState,
+      count: count != undefined ? (newState !== state ? count + 1 : count) : undefined
+    };
+  };
+
+  const deleteRetwit = async (state?: boolean, count?: number): Promise<handlerReturn> => {
+    const newState =
+      (await axiosTwits
+        .delete(`snaps/${twitId}`, {
+          params: {
+            retwit: true
+          },
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(() => !state)
+        .catch(() => {
+          return state;
+        })) === true;
+
+    return {
+      state: newState,
+      count: count != undefined ? (newState !== state ? count - 1 : count) : undefined
+    };
+  };
 
   return (
     <Interaction
@@ -20,9 +77,8 @@ export default function Retwit({
       icon_alt_color="rgb(47, 204, 110  )"
       initState={initState}
       initCount={initCount}
-      handler={async (state: boolean, count?: number): Promise<handlerReturn> => {
-        console.log('asd');
-        return { state: !state, count: count };
+      handler={async (state?: boolean, count?: number): Promise<handlerReturn> => {
+        return state ? deleteRetwit(state, count) : postRetwit(state, count);
       }}
     />
   );
