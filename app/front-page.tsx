83c94b874f47d84ab514743c2be523b2d3028fbd
Luis Escalante <lescalante@fi.uba.ar>
2024-12-04 23:55:01 -0300

    revert front page testing changes

--- app/front-page.tsx
+++ app/front-page.tsx
@@ -19,7 +19,7 @@ import useAxiosInstance from '@/hooks/useAxios';
 import { registerForPushNotificationsAsync } from '@/utils/notifications';
 
 import { authenticatedAtom } from './authAtoms/authAtom';
-import { UserAuth, UserSSORegisterDto } from './types/authTypes';
+import { UserSSORegisterDto } from './types/authTypes';
 
 GoogleSignin.configure({
   webClientId: '224360780470-maj4ma0cdjlm1o2376lv28m45rvm2e8e.apps.googleusercontent.com'
@@ -48,10 +48,7 @@ export default function FrontPage() {
           setIsLoadingSession(false);
           return;
         }
-        const userSession = JSON.parse(session) as UserAuth;
-        setAuthAtom(userSession);
-        router.replace(`/profile/${userSession.username}/edit`);
-        // router.replace('/biometric-login');
+        router.replace('/biometric-login');
       } else {
         setIsLoadingSession(false);
         // handleLocation(authAtom);
